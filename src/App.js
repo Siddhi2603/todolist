32aa021163c90ebf3d1c4fb173e2e1b0db124ee2
Siddhi2603 <ssiddhisonare26@gmail.com>
2023-05-21 16:44:06 +0530

    "Initial Commit"

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React,{useState} from 'react';
+import 'botstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './App.css';
+
+
+function App() {
+  return(
+    <div className= "ContainerApp">
+    <br></br>
+    <h2> To Do Liat App</h2>
+    <br></br>
+    siddhi sonare
+    
+    </div>
+  )
+};
+
+
+
+export default App;
+
+
+
+
